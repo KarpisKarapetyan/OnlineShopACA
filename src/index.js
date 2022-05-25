0c75214718eba43d2ce4e0daa3502d91c9db46ab
Emma <emma.tataryan2@gmail.com>
2022-05-25 21:46:17 +0400

    ET: Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import $ from 'jquery'
+import Popper from 'popper.js'
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import {Provider} from "react-redux"
 import store from "./redux/store"
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
