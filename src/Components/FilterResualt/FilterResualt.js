3c4ebc2f728eef3218f472c9ac8c75b2f1df8f55
Karpis Karapetyan <karpis.karpis93@gmail.com>
2022-05-26 13:34:59 +0400

    Chmerjel

--- src/Components/FilterResualt/FilterResualt.js
+++ src/Components/FilterResualt/FilterResualt.js
@@ -2,15 +2,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import { filterSelector } from '../../redux/slices/filterSlice'
 import './FilterResualt.css'
 
+
+
 function FilterResualt (){
     const filterData = useSelector(filterSelector)
     const dispatch = useDispatch()
 
-    console.log(filterData);
     return(
-        <div>
-           asd
-        </div>
+        <>
+ {filterData?.map((item, i) => (
+            <div key={i}>
+                <img src={item.img} />
+                <span>{item.price}</span>
+            </div>
+        ))}
+        </>
     )
+       
+    
 }
 export default FilterResualt
