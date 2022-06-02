b31e4fb9242fdd1b05624effd00702b4fe2589de
lvirabyan <102147659+lvirabyan@users.noreply.github.com>
2022-06-02 19:31:33 +0400

    add jacket

--- src/Components/KidsJackets/KidsJackets.js
+++ src/Components/KidsJackets/KidsJackets.js
@@ -3,45 +3,58 @@ import {useEffect, useState} from "react"
 import {mainUrl} from "../../api/api"
 import "./KidsJackets.css"
 import { useAddBasket } from "../../hooks/useAddBasket"
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useSelector } from 'react-redux'
+import { favoriteSelector, userBasketSelector } from '../../redux/slices/userSlice'
+import { useAddFavorite } from '../../hooks/useAddFavorite'
+import ImageZoom from '../HomePage/ImageZoom/ImageZoom'
 
 const KidsJackets = () => {
-
-    const [kidsJacketsArr, setKidsJacketsArr] = useState([])
+  const favoriteArr = useSelector(favoriteSelector)
+  const userBasket = useSelector(userBasketSelector)
+  const [kidsJacketsArr, setKidsJacketsArr] = useState([])
+  const goBasket = useAddBasket()
+  const goFavorite = useAddFavorite()
    
     useEffect(() => {
       axios.get(`${mainUrl}/kidsJackets`)
-      .then( console.log("================================================="))
       .then(res => setKidsJacketsArr(res.data))
+     
     }, []) 
-    const addBasket = useAddBasket()
+    
      
     return (
-      <div className="kidsJackets">
+      <>
+        <div className="kidsJackets">
          <span className='kidsJacketsTitle'> Kids Jacets</span>
-         <div className= "kidsJacketsData" >
-         {
-            kidsJacketsArr.map((item,i)=>{
+        </div>
+        {console.log("kidsJacketsArr-->",kidsJacketsArr)}
+        {
+           
+            kidsJacketsArr?.map((item,i)=>{
                 return(
-                    <div key={i}  className ="kidsJacketsItems">
-                        <div className="kidsJacketsDetailsImij">
-                          <img src={item.location} />
-                        </div>
-                        <div className='kidsJacketsDetails'
-                        >
-                          <button onClick={() => addBasket(item)}>Add Basket</button>
-                          <p> Size: {item.size} Price: {item.price} </p>
-                        </div>
-                      </div>
-                )
-            })
-        }
-         </div>
-       
-      </div>
+                    <div key={item.id} >
+                          <div >
+                              <img src={item.location} />
+                          </div>
+                       <div className='dressDetailsCarusel' >
+                                <div className='iconsCarusel' >
+                                    <label className='iconsCaruselBas' onClick={()=> goBasket(item)}> 
+                                    <AddShoppingCartIcon className={userBasket.includes(item) ? "activFavorite" : ''}/> </label>
+                                </div>
+                                <div className='iconsCarusel'> 
+                                    <label className='iconsCaruselFav' onClick={()=> goFavorite(item)}> 
+                                    <FavoriteIcon className={favoriteArr.includes(item) ? "activFavorite" : ''}/> </label>
+                                    <ImageZoom img = {kidsJacketsArr[i].location}/> 
+                                </div>
+                                <p > {item.price} AMD / {item.size}  </p>
+                </div>
+                    </div>
+                       )})
+          }
+        </>             
     )
-  }
+}
   
 export default KidsJackets
-  
-
-
